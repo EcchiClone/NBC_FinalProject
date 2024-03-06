09c0f57517b95dd1d7d7c688723c210b4ff70672
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-03-06 18:48:33 +0900

    Feat: 전체 Manager 관리 클래스 Managers 추가

---
+++ Assets/@Project/Scripts/Managers/Managers.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;
+    static Managers Instance { get { Init(); return s_instance; } }
+
+    #region # Core
+
+
+
+    #endregion
+
+    #region # Contents
+
+
+
+    #endregion
+
+
+
+    private static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject("@Managers");
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+
+    /// <summary>
+    /// 씬이 넘어갈 때 각 매니저의 초기화를 실행
+    /// </summary>
+    public void Clear()
+    {
+        // To-Do 초기화가 필요한 매니저들의 각 클래스에 Clear 함수를 이곳에서 호출.
+    }
+}
