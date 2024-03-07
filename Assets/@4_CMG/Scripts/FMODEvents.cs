857e512b312dff3bc91cf211a8e4178caf8b8673
najdorf <70437537+cn7249@users.noreply.github.com>
2024-03-07 12:21:49 +0900

    Feat: AudioManager & FMODEvents 기본 시스템 구축

---
+++ Assets/@4_CMG/Scripts/FMODEvents.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    // 아래와 같은 방식으로 fmod 내 이벤트를 가져와 사용
+    //[field: Header("Player SFX")]
+    //[field: SerializeField] public EventReference footstepsWalk { get; private set; }
+
+
+    public static FMODEvents instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Scene에 적어도 하나의 FMODEvents가 존재하는 지 확인하세요.");
+        }
+        instance = this;
+    }
+}
