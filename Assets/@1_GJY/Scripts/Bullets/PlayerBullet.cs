e098791026aef6eac4cad98200345f4ba057791f
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-03-14 15:53:48 +0900

    Feat: Player Primary Weapon 1번 로직 추가

--- Assets/@1_GJY/Scripts/Bullets/PlayerBullet.cs
+++ Assets/@1_GJY/Scripts/Bullets/PlayerBullet.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet_Temp : MonoBehaviour
+public class PlayerBullet : MonoBehaviour
 {
     public float speed;
 
     private void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnEnable()
+    {
+        Destroy(gameObject, 5f);
     }
 }
--- Assets/@1_GJY/Scripts/Weapon/Weapon_SingleCannon.cs
+++ Assets/@1_GJY/Scripts/Weapon/Weapon_SingleCannon.cs
@@ -8,9 +8,24 @@ public class Weapon_SingleCannon : Weapon_Primary
     {
         foreach (Transform muzzle in muzzlePoints)
         {
+            float xError = SetShotErrorRange();
+            float yError = SetShotErrorRange();
+
             GameObject bullet = Instantiate(WeaponSO.bulletPrefab);
             bullet.transform.position = muzzle.position;
             bullet.transform.rotation = muzzle.rotation;
-        }        
+
+            Quaternion rotation = Quaternion.Euler(yError, xError, 0f); // 각도 계산
+
+            bullet.transform.rotation *= rotation; // 현재 방향에 추가 회전을 적용
+        }
+    }
+
+    private float SetShotErrorRange(float standard = 0.5f)
+    {
+        float x1 = Random.Range(0f, 1f);
+        float x2 = Random.Range(0f, 1f);
+
+        return standard * (Mathf.Sqrt(-2.0f * Mathf.Log(x1)) * Mathf.Sin(2.0f * Mathf.PI * x2));
     }
 }
