c2256557844f7cde1970ed899a52ec3518335521
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-03-22 20:58:03 +0900

    Chore: 보스 몬스터 패턴 수치 조정

--- Assets/@2_LDH/Scripts/EnemyBullet.cs
+++ Assets/@2_LDH/Scripts/EnemyBullet.cs
@@ -6,17 +6,17 @@ public class EnemyBullet : Bullet
 {
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground") || other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
-        {
-            //Debug.Log(other.gameObject.layer);
-            EnemyBulletPoolManager.instance.OnReturnedToPool(gameObject);
-        }
-
         if (other.gameObject.TryGetComponent(out PlayerStateMachine playerState) == true)
         {
             playerState.Player.GetDamage(5);
 
             EnemyBulletPoolManager.instance.OnReturnedToPool(gameObject);
         }
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground") || other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            //Debug.Log(other.gameObject.layer);
+            EnemyBulletPoolManager.instance.OnReturnedToPool(gameObject);
+        }        
     }
 }
