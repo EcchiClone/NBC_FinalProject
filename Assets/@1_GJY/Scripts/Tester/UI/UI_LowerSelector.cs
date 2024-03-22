4c270ff13b8b4a92d2a20b7d5ea61c9a3b1ab03a
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-03-22 09:47:33 +0900

    Feat: MainUI 연동(완성1차)

--- Assets/@1_GJY/Scripts/Tester/UI/UI_LowerSelector.cs
+++ Assets/@1_GJY/Scripts/Tester/UI/UI_LowerSelector.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UI_LowerSelector : UI_Popup
 {
+    [SerializeField] GameObject _nextGroup;
+    [SerializeField] TextMeshProUGUI[] _specTexts;
+    [SerializeField] TextMeshProUGUI[] _nextSpecTexts;
     [SerializeField] private Transform _contents;
 
     enum Buttons
     {
         BackToSelector,
+    }
+
+    enum SpecType
+    {
+        AP,
+        Weight,
+        MoveSpeed,
+        JumpPower,
+        BoostPower,
     }
 
     protected override void Init()
@@ -18,7 +31,10 @@ public class UI_LowerSelector : UI_Popup
         int createUI = Managers.Module.LowerPartsCount;
 
         for (int i = 0; i < createUI; i++)
-            Managers.UI.ShowItemUI<UI_LowerChangeBtn>(_contents);
+            Managers.UI.ShowItemUI<UI_LowerChangeBtn>(_contents).SetParentUI(this);
+
+        Managers.Module.OnLowerChange += UpdateSelectedPartSpecText;
+        ResetText();
 
         BindButton(typeof(Buttons));
         GetButton((int)Buttons.BackToSelector).onClick.AddListener(BackToSelector);
@@ -26,7 +42,37 @@ public class UI_LowerSelector : UI_Popup
 
     private void BackToSelector()
     {
+        ResetText();
         _previousPopup.gameObject.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    private void ResetText()
+    {
+        LowerPart lower = Managers.Module.CurrentLowerPart;
+
+        UpdateSelectedPartSpecText(lower);
+    }
+
+    private void UpdateSelectedPartSpecText(LowerPart lower)
+    {
+        _nextGroup.SetActive(false);
+        _specTexts[(int)SpecType.AP].text = $"{lower.lowerSO.armor}";
+        _specTexts[(int)SpecType.Weight].text = $"{lower.lowerSO.weight}";
+
+        _specTexts[(int)SpecType.MoveSpeed].text = $"{lower.lowerSO.speed}";
+        _specTexts[(int)SpecType.JumpPower].text = $"{lower.lowerSO.jumpPower}";
+        _specTexts[(int)SpecType.BoostPower].text = $"{lower.lowerSO.boosterPower}";        
+    }
+
+    public void DisPlayNextPartSpecText(LowerPart nextLower)
+    {
+        _nextGroup.SetActive(true);
+        _nextSpecTexts[(int)SpecType.AP].text = $"{nextLower.lowerSO.armor}";
+        _nextSpecTexts[(int)SpecType.Weight].text = $"{nextLower.lowerSO.weight}";
+
+        _nextSpecTexts[(int)SpecType.MoveSpeed].text = $"{nextLower.lowerSO.speed}";
+        _nextSpecTexts[(int)SpecType.JumpPower].text = $"{nextLower.lowerSO.jumpPower}";
+        _nextSpecTexts[(int)SpecType.BoostPower].text = $"{nextLower.lowerSO.boosterPower}";
     }
 }
--- Assets/@1_GJY/Scripts/Tester/UI/UI_UpperChangeBtn.cs
+++ Assets/@1_GJY/Scripts/Tester/UI/UI_UpperChangeBtn.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -13,7 +12,7 @@ public class UI_UpperChangeBtn : UI_Item, IPointerEnterHandler, IPointerExitHand
     public int currentIndex;
     public static int IndexOfUpperPart = 0;
 
-    private UpperPart _currentPart;
+    private UpperPart _currentUpper;
 
     protected override void Init()
     {
@@ -25,13 +24,13 @@ public class UI_UpperChangeBtn : UI_Item, IPointerEnterHandler, IPointerExitHand
         Button button = GetComponent<Button>();
         button.onClick.AddListener(ChangePart);
 
-        _currentPart = Managers.Module.GetPartOfIndex<UpperPart>(currentIndex);
-        _partText.text = _currentPart.upperSO.display_Name;        
+        _currentUpper = Managers.Module.GetPartOfIndex<UpperPart>(currentIndex);
+        _partText.text = _currentUpper.upperSO.display_Name;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if(_parentUI._sidePopup == null)
+        if (_parentUI._sidePopup == null)
         {
             UI_PartsStatus info = Managers.UI.ShowPopupUI<UI_PartsStatus>();
             _parentUI.SetSidePopup(info);
@@ -39,7 +38,9 @@ public class UI_UpperChangeBtn : UI_Item, IPointerEnterHandler, IPointerExitHand
         }
 
         _parentUI._sidePopup.gameObject.SetActive(true);
-        Managers.Module.CallInfoChange(_currentPart.upperSO.display_Description);        
+        UI_UpperSelector selector = _parentUI as UI_UpperSelector;
+        selector.DisPlayNextPartSpecText(_currentUpper);
+        Managers.Module.CallInfoChange(_currentUpper.upperSO.display_Description);
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -50,6 +51,6 @@ public class UI_UpperChangeBtn : UI_Item, IPointerEnterHandler, IPointerExitHand
     private void ChangePart()
     {
         Managers.Module.ChangeUpperPart(currentIndex);
-        Managers.Module.CallUpperPartChange(_currentPart);
+        Managers.Module.CallUpperPartChange(_currentUpper);
     }
 }
