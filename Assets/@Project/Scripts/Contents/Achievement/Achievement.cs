b3904d4cc3d70666a55993b5ce92c4c427cf9599
EcchiClone <aa01092308481@gmail.com>
2024-04-02 21:42:52 +0900

    Chore: 업적 스크립팅, 4월 2일 체크포인트

--- Assets/@Project/Scripts/Contents/Achievement/Achievement.cs
+++ Assets/@Project/Scripts/Contents/Achievement/Achievement.cs
@@ -1,18 +1,205 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using UnityEngine;
 
-public class Achievement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using Debug = UnityEngine.Debug;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public enum QuestState
+{
+    Inactive,
+    Running,
+    Complete,
+    Cancel,
+    WaitingForCompletion
+}
+
+[CreateAssetMenu(menuName = "Achievement/Achievement", fileName = "Achievement_")]
+public class Achievement : ScriptableObject
+{
+    //#region Events
+    //public delegate void TaskSuccessChangedHandler(Achievement quest, AchievementTask task, int currentSuccess, int prevSuccess);
+    //public delegate void CompletedHandler(Achievement quest);
+    //public delegate void CanceledHandler(Achievement quest);
+    //public delegate void NewTaskGroupHandler(Achievement quest, TaskGroup currentTaskGroup, TaskGroup prevTaskGroup);
+    //#endregion
+
+    //[SerializeField]
+    //private TaskCategory category;
+    //[SerializeField]
+    //private Sprite icon;
+
+    //[Header("Text")]
+    //[SerializeField]
+    //private string codeName;
+    //[SerializeField]
+    //private string displayName;
+    //[SerializeField, TextArea]
+    //private string description;
+
+    //[Header("Task")]
+    //[SerializeField]
+    //private TaskGroup[] taskGroups;
+
+    //[Header("Reward")]
+    //[SerializeField]
+    //private AchievementReward[] rewards;
+
+    //[Header("Option")]
+    //[SerializeField]
+    //private bool useAutoComplete;
+    //[SerializeField]
+    //private bool isCancelable;
+    //[SerializeField]
+    //private bool isSavable;
+
+    //[Header("Condition")]
+    //[SerializeField]
+    //private AchievementCondition[] acceptionConditions;
+    //[SerializeField]
+    //private AchievementCondition[] cancelConditions;
+
+    //private int currentTaskGroupIndex;
+
+    //public TaskCategory Category => category;
+    //public Sprite Icon => icon;
+    //public string CodeName => codeName;
+    //public string DisplayName => displayName;
+    //public string Description => description;
+    //public QuestState State { get; private set; }
+    //public TaskGroup CurrentTaskGroup => taskGroups[currentTaskGroupIndex];
+    //public IReadOnlyList<TaskGroup> TaskGroups => taskGroups;
+    //public IReadOnlyList<AchievementReward> Rewards => rewards;
+    //public bool IsRegistered => State != QuestState.Inactive;
+    //public bool IsComplatable => State == QuestState.WaitingForCompletion;
+    //public bool IsComplete => State == QuestState.Complete;
+    //public bool IsCancel => State == QuestState.Cancel;
+    //public virtual bool IsCancelable => isCancelable && cancelConditions.All(x => x.IsPass(this));
+    //public bool IsAcceptable => acceptionConditions.All(x => x.IsPass(this));
+    //public virtual bool IsSavable => isSavable;
+
+    //public event TaskSuccessChangedHandler onTaskSuccessChanged;
+    //public event CompletedHandler onCompleted;
+    //public event CanceledHandler onCanceled;
+    //public event NewTaskGroupHandler onNewTaskGroup;
+
+    //public void OnRegister()
+    //{
+    //    Debug.Assert(!IsRegistered, "This quest has already been registered.");
+
+    //    foreach (var taskGroup in taskGroups)
+    //    {
+    //        taskGroup.Setup(this);
+    //        foreach (var task in taskGroup.Tasks)
+    //            task.onSuccessChanged += OnSuccessChanged;
+    //    }
+
+    //    State = QuestState.Running;
+    //    CurrentTaskGroup.Start();
+    //}
+
+    //public void ReceiveReport(string category, object target, int successCount)
+    //{
+    //    Debug.Assert(IsRegistered, "This quest has already been registered.");
+    //    Debug.Assert(!IsCancel, "This quest has been canceled.");
+
+    //    if (IsComplete)
+    //        return;
+
+    //    CurrentTaskGroup.ReceiveReport(category, target, successCount);
+
+    //    if (CurrentTaskGroup.IsAllTaskComplete)
+    //    {
+    //        if (currentTaskGroupIndex + 1 == taskGroups.Length)
+    //        {
+    //            State = QuestState.WaitingForCompletion;
+    //            if (useAutoComplete)
+    //                Complete();
+    //        }
+    //        else
+    //        {
+    //            var prevTasKGroup = taskGroups[currentTaskGroupIndex++];
+    //            prevTasKGroup.End();
+    //            CurrentTaskGroup.Start();
+    //            onNewTaskGroup?.Invoke(this, CurrentTaskGroup, prevTasKGroup);
+    //        }
+    //    }
+    //    else
+    //        State = QuestState.Running;
+    //}
+
+    //public void Complete()
+    //{
+    //    CheckIsRunning();
+
+    //    foreach (var taskGroup in taskGroups)
+    //        taskGroup.Complete();
+
+    //    State = QuestState.Complete;
+
+    //    foreach (var reward in rewards)
+    //        reward.Give(this);
+
+    //    onCompleted?.Invoke(this);
+
+    //    onTaskSuccessChanged = null;
+    //    onCompleted = null;
+    //    onCanceled = null;
+    //    onNewTaskGroup = null;
+    //}
+
+    //public virtual void Cancel()
+    //{
+    //    CheckIsRunning();
+    //    Debug.Assert(IsCancelable, "This quest can't be canceled");
+
+    //    State = QuestState.Cancel;
+    //    onCanceled?.Invoke(this);
+    //}
+
+    //public Achievement Clone()
+    //{
+    //    var clone = Instantiate(this);
+    //    clone.taskGroups = taskGroups.Select(x => new TaskGroup(x)).ToArray();
+
+    //    return clone;
+    //}
+    //public QuestSaveData ToSaveData()
+    //{
+    //    return new QuestSaveData
+    //    {
+    //        codeName = codeName,
+    //        state = State,
+    //        taskGroupIndex = currentTaskGroupIndex,
+    //        taskSuccessCounts = CurrentTaskGroup.Tasks.Select(x => x.CurrentSuccess).ToArray()
+    //    };
+    //}
+    //public void LoadFrom(QuestSaveData saveData)
+    //{
+    //    State = saveData.state;
+    //    currentTaskGroupIndex = saveData.taskGroupIndex;
+    //    for (int i = 0; i < currentTaskGroupIndex; i++)
+    //    {
+    //        var taskGroup = taskGroups[i];
+    //        taskGroup.Start();
+    //        taskGroup.Complete();
+    //    }
+    //    for (int i = 0; i < saveData.taskSuccessCounts.Length; i++)
+    //    {
+    //        CurrentTaskGroup.Start();
+    //        CurrentTaskGroup.Tasks[i].CurrentSuccess = saveData.taskSuccessCounts[i];
+    //    }
+    //}
+
+    //private void OnSuccessChanged(Task task, int currentSuccess, int prevSuccess)
+    //    => onTaskSuccessChanged?.Invoke(this, task, currentSuccess, prevSuccess);
+
+    //[Conditional("UNITY_EDITOR")]
+    //private void CheckIsRunning()
+    //{
+    //    Debug.Assert(IsRegistered, "This quest has already been registered");
+    //    Debug.Assert(!IsCancel, "This quest has been canceled.");
+    //    Debug.Assert(!IsComplete, "This quest has already been completed");
+    //}
 }
