6bbfc0deedfb881605a8e2ea6a515143d4d4184d
najdorf <70437537+cn7249@users.noreply.github.com>
2024-04-04 16:21:41 +0900

    Feat: LineRenderer 반짝이게 함

--- Assets/@Project/Scripts/Contents/Perk/CameraController.cs
+++ Assets/@Project/Scripts/Contents/Perk/CameraController.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
+    [Header ("Define")]
     private Transform _cameraHolder;
-
+    private Camera _mainCamera;
     private float _speed;
+
+    [Header ("Zoom")]
+    [SerializeField] private float _zoomSensitivity;
+    private float _scrollY;
+    private float _zoomAmount;
 
     private void Awake()
     {
         _cameraHolder = transform;
+        _mainCamera = GetComponentInChildren<Camera>();
         _speed = 50.0f;
+        _zoomAmount = 45.0f;
+        _zoomSensitivity = 0.05f;
     }
 
     private void Update()
     {
         CameraMovement();
+        CameraZoom();
+    }
+
+    private void LateUpdate()
+    {
+        
     }
 
     private void CameraMovement()
@@ -37,5 +54,28 @@ public class CameraController : MonoBehaviour
         {
             _cameraHolder.position += new Vector3(_speed, 0f, 0f);
         }
+    }
+
+    private void CameraZoom()
+    {
+        if (_scrollY > 0 && _mainCamera.fieldOfView <= 30)
+        {
+            _mainCamera.fieldOfView = 30;
+        }
+        else if (_scrollY < 0 && _mainCamera.fieldOfView >= 60)
+        {
+            _mainCamera.fieldOfView = 60;
+        }
+        else
+        {
+            _zoomAmount += -_scrollY * _zoomSensitivity;
+            _mainCamera.fieldOfView = Mathf.Clamp(_zoomAmount, 30, 60);
+        }
+    }
+
+    public void OnZoomInput(InputAction.CallbackContext context)
+    {
+        _scrollY = context.ReadValue<float>();
+        Debug.Log(_scrollY);
     }
 }
