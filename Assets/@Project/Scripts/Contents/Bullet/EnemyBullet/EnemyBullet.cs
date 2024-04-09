269b72edbd194c5e85e5d5efacff10c404ad5d9b
EcchiClone <aa01092308481@gmail.com>
2024-04-09 16:30:45 +0900

    Chore: 플레이어 탐색 조건 개선, 씬 파일 이름 변경(테스트용 씬), 불필요 씬 삭제,

--- Assets/@Project/Scripts/Contents/Bullet/EnemyBullet/EnemyBullet.cs
+++ Assets/@Project/Scripts/Contents/Bullet/EnemyBullet/EnemyBullet.cs
@@ -14,13 +14,21 @@ public class EnemyBullet : Bullet
             ObjectPooler.SpawnFromPool("Test_Copied_Explosion01_Effect", transform.position);
             gameObject.SetActive(false);
         }
-
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground") || other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Ground") || other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
         {
             //Debug.Log(other.gameObject.layer);
             //EnemyBulletPoolManager.instance.OnReturnedToPool(gameObject);
             ObjectPooler.SpawnFromPool("Test_Copied_Explosion01_Effect", transform.position);
             gameObject.SetActive(false);
-        }        
+        }
+        else if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("플레이어 태그를 가진 오브젝트에게 데미지를 주는 로직 대신 Debug Log를 실행하고 있습니다.");
+
+            //EnemyBulletPoolManager.instance.OnReturnedToPool(gameObject);
+            ObjectPooler.SpawnFromPool("Test_Copied_Explosion01_Effect", transform.position);
+            gameObject.SetActive(false);
+        }
+  
     }
 }
