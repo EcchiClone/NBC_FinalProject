aad38dc7bf99a91d91c5502514abd6d620ca9401
EcchiClone <aa01092308481@gmail.com>
2024-04-09 17:56:04 +0900

    Feat: 몬스터 파괴 시 업적진행도 업데이트용 코드 수정

--- Assets/@Project/Scripts/Contents/Achievement/Updater/AchievementUpdater.cs
+++ Assets/@Project/Scripts/Contents/Achievement/Updater/AchievementUpdater.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class AchievementUpdater : MonoBehaviour
 {
     [SerializeField] protected TaskCategory taskCategory;
     [SerializeField] protected TaskTarget taskTarget;
-    protected int value;
     protected void Report()
+    {
+        AchievementSystem.Instance.ReceiveReport(taskCategory, taskTarget, 1);
+    }
+    protected void Report(int value)
     {
         AchievementSystem.Instance.ReceiveReport(taskCategory, taskTarget, value);
     }
