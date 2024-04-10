8f1a47e7852ba3a8501b27f06fa4d688296c2c8d
najdorf <70437537+cn7249@users.noreply.github.com>
2024-04-10 13:38:21 +0900

    Feat: LineDrawer 스크립트 함축 및 기능 이동
    대상:
    Tier1PerkLineDrawer,
    Tier2PerkLineDrawer,
    Tier3PerkLineDrawer
    => PerkLineDrawer 및 PerkVarBehaviour에 통합

--- Assets/@Project/Scripts/Contents/Perk/PerkVarBehaviour.cs
+++ Assets/@Project/Scripts/Contents/Perk/PerkVarBehaviour.cs
@@ -4,48 +4,93 @@ using UnityEngine;
 
 public class PerkVarBehaviour : MonoBehaviour
 {
+    private PerkLineDrawer _drawer;
+
     private PerkTier _tier;
     private int _idx;
 
     private PerkInfo _perkInfo;
     private int _contentIdx;
-    private bool _isActive;
 
     private ContentInfo _contentInfo;
-    private string _name;
-    private string _description;
 
-    public float distance;
+    private float _distance;
+    private Vector3 _prevPosition;
 
     private void Awake()
     {
+        _drawer = GetComponent<PerkLineDrawer>();
         GetVarsFromManager();
     }
 
     private void Start()
     {
-        GetInfosFromManager();
-        GetContentsFromManager();
+        FindPrevPerks(_perkInfo.Tier, ref _distance, ref _prevPosition);
+        DrawLineToPrevPerk();
     }
 
     private void GetVarsFromManager()
     {
         _tier = PerkManager.Instance.PointerTier;
         _idx = PerkManager.Instance.PointerIdx;
+        GetInfosFromManager();
+        GetContentsFromManager();
     }
 
     private void GetInfosFromManager()
     {
         _perkInfo = PerkManager.Instance.GetPerkInfo(_tier, _idx);
         _contentIdx = _perkInfo.ContentIdx;
-        _isActive = _perkInfo.IsActive;
     }
 
     private void GetContentsFromManager()
     {
         _contentInfo = PerkManager.Instance.GetContentInfo(_tier, _contentIdx);
-        _name = _contentInfo.name;
-        _description = _contentInfo.description;
+    }
+
+    private void FindPrevPerks(PerkTier tier, ref float distance, ref Vector3 prevPosition)
+    {
+        GameObject[] prevPerks;
+
+        switch (tier)
+        {
+            case PerkTier.TIER3: 
+                prevPerks = GameObject.FindGameObjectsWithTag("Tier2");
+                break;
+            case PerkTier.TIER2:
+                prevPerks = GameObject.FindGameObjectsWithTag("Tier1");
+                break;
+            default:
+                prevPerks = null;
+                break;
+        }
+
+        if (prevPerks != null)
+        {
+            float min = 0f;
+
+            foreach (GameObject perk in prevPerks)
+            {
+                distance = Vector3.Distance(perk.transform.position, transform.position);
+
+                if (min == 0f || distance < min)
+                {
+                    min = distance;
+                    prevPosition = perk.transform.position;
+                }
+            }
+        }
+        else
+        {
+            distance = 0f;
+            prevPosition = Vector3.zero;
+        }
+
+    }
+
+    private void DrawLineToPrevPerk()
+    {
+        _drawer.LineToPrevPerk(_prevPosition);
     }
 
     public PerkInfo ReturnPerkInfo()
@@ -56,5 +101,10 @@ public class PerkVarBehaviour : MonoBehaviour
     public ContentInfo ReturnContentInfo()
     {
         return _contentInfo;
+    }
+
+    public float ReturnPrevDistance()
+    {
+        return _distance;
     }
 }
